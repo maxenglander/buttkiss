bc7fc18804eca3325c5cc5df4a09a2eab68b2e80
Max Englander <menglander@sailthru.com>
2013-12-25 20:31:38 -0500

    evaluateJSX upon OSC event

--- main.js
+++ main.js
@@ -8,20 +8,42 @@
         MENU_ID = "buttkiss",
         MENU_LABEL = "$$$/JavaScripts/Generator/Buttkiss/Menu=Buttkiss";
 
+    function startOscServer() {
+        console.log("starting osc server");
+        var inport, osc, sock, udp;
+        osc = require('osc-min');
+        udp = require("dgram");
+
+        inport = 41234;
+
+        sock = udp.createSocket("udp4", function(msg, rinfo) {
+          var error;
+          try {
+            console.log(osc.fromBuffer(msg));
+            var flipColorsExtendScript = "var color = app.foregroundColor; color.rgb.red = 255 * Math.random(); color.rgb.green = 255 * Math.random(); color.rgb.blue = 255 * Math.random(); app.foregroundColor = color;";
+            _generator.evaluateJSXString(flipColorsExtendScript);
+          } catch (_error) {
+            error = _error;
+            return console.log("invalid OSC packet");
+          }
+        });
+
+        sock.bind(inport);
+    }
+
     function init(generator, config) {
         _generator = generator;
         _config = config;
+        _generator.addMenuItem(MENU_ID, MENU_LABEL, true, false);
 
-        console.log("initializing generator buttkiss with config %j", _config);
+        function initLater() {
+           startOscServer();
+        }
 
-        _generator.addMenuItem(MENU_ID, MENU_LABEL, true, false).then(
-            function () {
-                console.log("Menu created", MENU_ID);
-            }, function () {
-                console.error("Menu creation failed", MENU_ID);
-            }
-        );
+        process.nextTick(initLater);
     }
 
     exports.init = init;
 }());
+
+
